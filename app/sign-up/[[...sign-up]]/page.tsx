bbea95411c292e21ab43b0f4ef47bf30504bd55f
Chinmay1635 <chinmaykulkarni165@gmail.com>
2025-09-15 10:44:13 +0530

    Revert "Convert React App to Next.js with Backend"
    This reverts commit 1a36c8ca1f13c6be4a71d3e8c553fb90811dd067.

--- app/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from '@clerk/nextjs'
-
-export default function Page() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <SignUp />
-    </div>
-  )
-}
